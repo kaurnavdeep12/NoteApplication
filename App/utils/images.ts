2d01c1568f8ba82aa21917669998c441462a1dc2
Zapbuild <zapbuild@Zapbuilds-iMac.local>
2021-12-09 12:35:18 +0530

    Delete extra images

--- App/utils/images.ts
+++ App/utils/images.ts
@@ -1,10 +1,5 @@
 export const images = {
   SplashNew: require('../assets/images.jpg'),
   Splash: require('../assets/black.jpg'),
-  Congrats: require('../assets/congrats.jpg'),
-  logout: require('../assets/logout.png'),
-  vector: require('../assets/imager.svg'),
-  Loogot: require('../assets/Log.png'),
-  add_icon: require('../assets/add.png'),
   notes: require('../assets/Notes.png'),
 };
